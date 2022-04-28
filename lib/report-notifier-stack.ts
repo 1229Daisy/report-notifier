f0218c1526d294ddbca70295aaa2572a65ec4565
1229Daisy <2517022100@qq.com>
2022-04-28 15:30:11 +0800

    lxy

--- lib/report-notifier-stack.ts
+++ lib/report-notifier-stack.ts
@@ -14,8 +14,8 @@ export class ReportNotifierStack extends Stack {
 
         const sns_topic = new Topic(this, 'epi_reports_topic',);
         console.log(__dirname);
-
-        const config = JSON.parse(readFileSync(__dirname+"/../config/config.json").toString());
+        console.info(readFileSync(__dirname+"/../config/config.json",'utf8').toString())
+        const config = JSON.parse(readFileSync(__dirname+"/../config/config.json",'utf8').toString());
         config['subscribers'].forEach((subscriber: any) => {
             const emailFilter={
                 "send_to":SubscriptionFilter.stringFilter({
